b1cde1ca98f73a6af67466f3116adbe8a3a46eed
Joaogdiass <joaogdemello@outlook.com>
2024-02-19 20:56:48 -0300

    Primeiro commit Screen Match

---
+++ CursoAlura2/src/CursoAlura2/Filme.java
@@ -0,0 +1,10 @@
+package CursoAlura2;
+
+public class Filme {
+	String nome;
+	int anoDeLancamento;
+	boolean incluidoNoPlano;
+	double avaliacao;
+	int totalDeAvaliacoes;
+	int duracaoEmMinutos;
+}
