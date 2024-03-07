8f5f0f4ab7411966aa1711ea86b0e536f3b33a14
Joaogdiass <joaogdemello@outlook.com>
2024-03-07 15:46:28 -0300

    coleta de dados e lista teorico finalizado 2

---
+++ CursoAlura2/src/br/com/alura/ScreenMatch/principal/PrincipalComListas.java
@@ -0,0 +1,36 @@
+package br.com.alura.ScreenMatch.principal;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import br.com.alura.ScreenMatch.modelos.Filme;
+import br.com.alura.ScreenMatch.modelos.Serie;
+import br.com.alura.ScreenMatch.modelos.Titulo;
+
+public class PrincipalComListas {
+	
+	public static void main(String[] args) {
+		
+		Filme meuFilme = new Filme("O poderoso Chefao",1970);
+		meuFilme.avalia(8);
+		var filmeDoPaulo = new Filme("Dogville",2003);
+		filmeDoPaulo.avalia(5);
+		Serie serie = new Serie("lost",2000);
+		serie.avalia(2);
+		
+		ArrayList<Titulo> lista = new ArrayList<>();
+		lista.add(filmeDoPaulo);
+		lista.add(meuFilme);
+		lista.add(serie);
+		for (Titulo item : lista) {
+			System.out.println(item.getNome());
+			if(item instanceof Filme filme && filme.getClassificacao() >2) {
+				//Filme filme = (Filme) item;
+				System.out.println("Classificacao: " + filme.getClassificacao());
+				
+			}
+			
+		}
+		
+	}
+}
